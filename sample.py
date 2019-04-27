e8b2ce921af0646ea6effbfd6ee32d7c99817e84
karthik2403 <karthik2403@outlook.com>
2019-04-27 18:04:55 +0530

    waste commit

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
 print 'Hi this is first program to commit with repository'
 print 'This is the secound line for secound commit'
 print 'This is third line, and now i am pushing from pycharm'
+print 'This is waste line and commited line.'
